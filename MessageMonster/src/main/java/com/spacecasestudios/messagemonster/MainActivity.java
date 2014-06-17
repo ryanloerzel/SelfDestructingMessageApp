192eedfde73986f65fbcca1a8a23ea422658c666
ryanloerzel <ryan.loerzel@gmail.com>
2014-06-16 20:37:04 -0700

    Added the Edit Friends screen, added functionality to query Parse.com and create friend relationships.  Displayed list of friends in the Friends Fragment activity

--- MessageMonster/src/main/java/com/spacecasestudios/messagemonster/MainActivity.java
+++ MessageMonster/src/main/java/com/spacecasestudios/messagemonster/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import com.parse.ParseUser;
 
@@ -36,6 +37,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_main);
 
         ParseUser currentUser = ParseUser.getCurrentUser();
@@ -106,6 +108,10 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
         if (itemId == R.id.action_logout) {
             ParseUser.logOut();
             navigateToLogin();
+        }
+        else if (itemId == R.id.action_edit_friends) {
+            Intent intent = new Intent(this,EditFriendsActivity.class);
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ MessageMonster/src/main/java/com/spacecasestudios/messagemonster/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.spacecasestudios.messagemonster;
+
+/**
+ * Created by Ryan on 6/16/2014.
+ */
+public final class ParseConstants {
+    //Class names
+
+    //Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
