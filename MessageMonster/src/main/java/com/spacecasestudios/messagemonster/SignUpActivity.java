31fff180f86b889a7661519d4f883647c8cdbfb8
ryanloerzel <ryan.loerzel@gmail.com>
2014-06-28 22:14:23 -0700

    updated the login UI

--- MessageMonster/src/main/java/com/spacecasestudios/messagemonster/SignUpActivity.java
+++ MessageMonster/src/main/java/com/spacecasestudios/messagemonster/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.spacecasestudios.messagemonster;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
@@ -25,6 +26,9 @@ public class SignUpActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_sign_up);
+
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
 
         mUserName = (EditText) findViewById(R.id.usernameField);
         mPassword = (EditText)findViewById(R.id.passwordField);
