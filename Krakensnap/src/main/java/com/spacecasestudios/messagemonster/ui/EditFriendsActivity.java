41fa41dd058fb050ce8ec3f20c3509fbba9810f3
ryanloerzel <ryan.loerzel@gmail.com>
2014-08-26 21:03:36 -0700

    Major Work on EditFriendsActivity.java: In the process of implementing a check to require email validation when adding a friend/contact; TODO: check mark should only appear when emails match and contact should be added to the contact list.

--- Krakensnap/src/main/java/com/spacecasestudios/messagemonster/ui/EditFriendsActivity.java
+++ Krakensnap/src/main/java/com/spacecasestudios/messagemonster/ui/EditFriendsActivity.java
@@ -8,8 +8,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,26 +30,35 @@ import java.util.List;
 
 public class EditFriendsActivity extends Activity {
 
+    //Declare member variables
     public static final String TAG = EditFriendsActivity.class.getSimpleName();
     protected List<ParseUser> mUsers;
     protected ParseRelation<ParseUser> mFriendsRelation;
     protected ParseUser mCurrentUser;
     protected GridView mGridView;
+    protected TextView mContactEmail;
+    protected Button mValidateEmail;
+    protected String mEmail;
+    protected LinearLayout mLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-        setContentView(R.layout.user_grid);
+        setContentView(R.layout.user_grid_edit_friends);
 
-        //setupActionBar();
-
-        mGridView = (GridView)findViewById(R.id.friendsGrid);
+        //Initialize member variables
+        mGridView = (GridView) findViewById(R.id.friendsGrid);
         mGridView.setChoiceMode(GridView.CHOICE_MODE_MULTIPLE);
         mGridView.setOnItemClickListener(mOnItemClickListener);
 
-        TextView emptyTextView = (TextView)findViewById(android.R.id.empty);
+        TextView emptyTextView = (TextView) findViewById(android.R.id.empty);
         mGridView.setEmptyView(emptyTextView);
+
+        mLayout = (LinearLayout) findViewById(R.id.linear_layout);
+        mContactEmail = (TextView) findViewById(R.id.contactEmail);
+        mValidateEmail = (Button) findViewById(R.id.validateButton);
 
     }
 
@@ -60,14 +71,19 @@ public class EditFriendsActivity extends Activity {
         //*********************************************************************************
         //A null pointer exception was being thrown without this try-catch block
         //TODO: figure out what is causing the exception
-        try{
+        try {
             mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             Log.e(TAG, e.getMessage());
         }
         //***********************************************************************************
 
+        //***********************************************************************************
+        //Retrieve a List of of ParseUser Objects from the parse.com, sorted by username
+        //Set the progress bar to visible while loading and invisible when finished
+        //Assign the ParseUser List to the mUsers variable and create an array of user names
+        //Set the Grid view to hold the Parse user names
+        //***********************************************************************************
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);
         query.setLimit(1000);
@@ -76,34 +92,32 @@ public class EditFriendsActivity extends Activity {
             @Override
             public void done(List<ParseUser> users, ParseException e) {
                 setProgressBarIndeterminateVisibility(false);
-                if(e == null){
+                if (e == null) {
                     //success
                     mUsers = users;
                     String[] usernames = new String[mUsers.size()];
                     int i = 0;
-                    for(ParseUser user: mUsers){
+                    for (ParseUser user : mUsers) {
                         usernames[i] = user.getUsername();
                         i++;
                     }
-                    if (mGridView.getAdapter() == null){
+                    if (mGridView.getAdapter() == null) {
                         UserAdapter adapter = new UserAdapter(EditFriendsActivity.this, mUsers);
                         mGridView.setAdapter(adapter);
-                    }
-                    else {
-                        ((UserAdapter)mGridView.getAdapter()).refill(mUsers);
+                    } else {
+                        ((UserAdapter) mGridView.getAdapter()).refill(mUsers);
                     }
 
                     addFriendCheckmarks();
 
 
-                }
-                else {
+                } else {
                     Log.e(TAG, e.getMessage());
                     AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
                     builder.setMessage(e.getMessage())
                             .setTitle(R.string.error_title)
                             .setPositiveButton(android.R.string.ok, null);
-                    AlertDialog dialog= builder.create();
+                    AlertDialog dialog = builder.create();
                     dialog.show();
                 }
             }
@@ -126,19 +140,18 @@ public class EditFriendsActivity extends Activity {
         mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
             @Override
             public void done(List<ParseUser> friends, ParseException e) {
-                if(e == null){
+                if (e == null) {
                     //list returned look for match
-                    for(int i = 0; i < mUsers.size(); i++){
+                    for (int i = 0; i < mUsers.size(); i++) {
                         ParseUser user = mUsers.get(i);
 
-                        for(ParseUser friend : friends){
-                            if(friend.getObjectId().equals(user.getObjectId())){
+                        for (ParseUser friend : friends) {
+                            if (friend.getObjectId().equals(user.getObjectId())) {
                                 mGridView.setItemChecked(i, true);
                             }
                         }
                     }
-                }
-                else{
+                } else {
                     Log.e(TAG, e.getMessage());
                 }
             }
@@ -148,30 +161,92 @@ public class EditFriendsActivity extends Activity {
     protected AdapterView.OnItemClickListener mOnItemClickListener = new AdapterView.OnItemClickListener() {
         @Override
         public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-            ImageView checkImageView = (ImageView)view.findViewById(R.id.checkImageView);
+            ImageView checkImageView = (ImageView) view.findViewById(R.id.checkImageView);
 
-            if( mGridView.isItemChecked(position)){
-                String name = mUsers.get(position).getUsername();
+            if (mGridView.isItemChecked(position)) {
+                String email = mUsers.get(position).getString("email");
+                setEmail(email);
                 mFriendsRelation.add(mUsers.get(position));
                 checkImageView.setVisibility(View.VISIBLE);
-                Toast.makeText(EditFriendsActivity.this, "Checked!"+ name, Toast.LENGTH_LONG).show();
-            }
-            else{
+                //Toast.makeText(EditFriendsActivity.this, "Checked! "+ email, Toast.LENGTH_SHORT).show();
+            } else {
                 //remove
                 mFriendsRelation.remove(mUsers.get(position));
                 checkImageView.setVisibility(View.INVISIBLE);
-                Toast.makeText(EditFriendsActivity.this, "Not Checked!", Toast.LENGTH_LONG).show();
+                Toast.makeText(EditFriendsActivity.this, "Not Checked!", Toast.LENGTH_SHORT).show();
             }
 
             mCurrentUser.saveInBackground(new SaveCallback() {
                 @Override
                 public void done(ParseException e) {
-                    if(e != null){
+                    if (e != null) {
                         Log.e(TAG, e.getMessage());
                     }
                 }
             });
         }
+
+        private void setEmail(final String contactEmail) {
+            mGridView.setVisibility(View.INVISIBLE);
+            mLayout.setVisibility(View.VISIBLE);
+            mContactEmail.setVisibility(View.VISIBLE);
+            mValidateEmail.setVisibility(View.VISIBLE);
+
+            mValidateEmail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mEmail = mContactEmail.getText().toString();
+                    if (mEmail.equals(contactEmail)) {
+                        Toast.makeText(EditFriendsActivity.this, "Match! " + contactEmail, Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        Toast.makeText(EditFriendsActivity.this, "Bummer! " + contactEmail, Toast.LENGTH_SHORT).show();
+                    }
+                    mContactEmail.setVisibility(View.INVISIBLE);
+                    mValidateEmail.setVisibility(View.INVISIBLE);
+                    mLayout.setVisibility(View.INVISIBLE);
+                    mGridView.setVisibility(View.VISIBLE);
+                }
+            });
+        }
+
     };
 
 }
+
+
+
+
+
+
+
+//        private String emailValidation() {
+//            final String[] google = new String[1];
+//            AlertDialog.Builder alert = new AlertDialog.Builder(EditFriendsActivity.this);
+//
+//
+//            alert.setTitle("Add a contact");
+//            alert.setMessage("Please enter this contact's email address to add");
+//
+//            // Set an EditText view to get user input
+//            final EditText input = new EditText(EditFriendsActivity.this);
+//            alert.setView(input);
+//
+//
+//            alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int whichButton) {
+//                    String value = String.valueOf(input.getText());
+//                    google[0] = value;
+//                }
+//            });
+//
+//            alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                public void onClick(DialogInterface dialog, int whichButton) {
+//                    // Canceled.
+//                }
+//            });
+//
+//            alert.show();
+//            return google[0];
+//
+//        }
